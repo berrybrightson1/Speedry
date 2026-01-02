69c185ad618a2118a572254afb127d5bcb25ed50
berrybrightson1 <berrybrightson@gmail.com>
2026-01-02 18:50:28 +0000

    Fix: Remove duplicate prop in MenuScreen

--- app/page.tsx
+++ app/page.tsx
@@ -405,7 +405,6 @@ export default function SpeedryConquest() {
             bestLevel={bestLevel}
             xp={xp}
             onOpenStore={() => setShowGlobalStore(true)}
-            onOpenStore={() => setShowGlobalStore(true)}
           />
         )}
 
